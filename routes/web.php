0e69a3c47693395c5ed676e779c6126483336479
newbieflank <raninovia188@gmail.com>
2025-02-24 09:42:25 +0700

    anggota - done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,5 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('admin');
 })->name('admin');
+
+Route::resource('member', MemberController::class);
